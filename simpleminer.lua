68b56250c76d9f5af2d50faafc73c4c2f8fa5f01
Eero Vilpponen <eero.vilpponen@gmail.com>
2025-10-18 18:07:13 +0300

    Refuel

--- simpleminer.lua
+++ simpleminer.lua
@@ -8,9 +8,35 @@ local function safeForward()
 end
 
 
+local function refuel()
+    local goodFuel = {"minecraft:coal", "minecraft:charcoal"}
+    -- jos polttoainetta on alle 500, yritä tankata
+    if turtle.getFuelLevel() < 500 then
+        print("Polttoainetta vähän, yritetään tankata...")
+        for slot = 1, 16 do
+            turtle.select(slot)
+            local itemCount = turtle.getItemCount(slot)
+            if itemCount > 0 then
+                local itemDetail = turtle.getItemDetail(slot)
+                if itemDetail.name == goodFuel[1] or itemDetail.name == goodFuel[2] then
+                    turtle.refuel()
+                    print("Tankattu " .. itemCount .. " kappaletta " .. itemDetail.name)
+                    if turtle.getFuelLevel() >= 1000 then
+                        print("Polttoaine riittää nyt.")
+                        return
+                    end
+                end
+            end
+        end
+        print("Ei löytynyt polttoainetta tankattavaksi.")
+    end
+end
+
+
 -- Pääsilmukka: kaiva tunneli eteenpäin 5 blokkia (2 korkuinen)
 -- sitten käänny suuntaan jossa on blokkeja. Jatka siitä
 while true do
+    refuel()
     for i = 1, 5 do
         -- Kaiva edessä
         turtle.dig()
