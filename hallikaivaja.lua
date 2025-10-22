be2183d3d753ae54c1a42fed9da8280b1e1d5edc
Eero Vilpponen <eero.vilpponen@gmail.com>
2025-10-22 14:16:53 +0300

    nice hallikaivaja

--- hallikaivaja.lua
+++ hallikaivaja.lua
@@ -2,57 +2,34 @@
 local utils = dofile("lib/utils.lua")
 local Actions = dofile("lib/actions.lua")
 
--- local function zigzagStep(step)
---     -- eteenpäin
---     turtle.dig()
---     utils.safeForward()
---     -- oikealle
---     turtle.turnRight()
---     turtle.dig()
---     utils.safeForward()
---     -- vasemmalle
---     turtle.turnLeft()
---     turtle.dig()
---     utils.safeForward()
---     -- vasemmalle
---     turtle.turnLeft()
---     turtle.dig()
---     utils.safeForward()
---     -- oikealle
---     turtle.turnRight()
--- end
 
--- Pääsilmukka: tee 3x3 neliö
+local PITUUS=10
+
+-- Pääsilmukka: kaiva 2 korkuista tunnelia x blokkia, sitten käänny 180 astetta ja toista
 while true do
     tracker = Actions.new("hallikaivaja")
     tracker:cycle(function()
-        -- kaiva 3 eteenpäin
-        for i = 1, 3 do
+        -- aloita "uusi" seinän poisto
+        turtle.dig()
+        turtle.digUp()
+        turtle.turnRight()
+        -- kaiva 10 eteenpäin ja ylös
+        for i = 1, (PITUUS - 1) do
             turtle.dig()
+            turtle.digUp()
             tracker:moveForward()
         end
         -- käänny oikealle
-        tracker:turnRight()
-        -- kaiva 3 eteenpäin
-        for i = 1, 3 do
+        tracker:turnLeft()
+        turtle.dig()
+        tracker:moveForward()
+        tracker:turnLeft()
+        -- kaiva 10 eteenpäin ja ylös
+        for i = 1, (PITUUS - 1) do
             turtle.dig()
+            turtle.digUp()
             tracker:moveForward()
         end
-        -- käänny oikealle
-        tracker:turnRight()
-        -- kaiva 3 eteenpäin
-        for i = 1, 3 do
-            turtle.dig()
-            tracker:moveForward()
-        end
-        -- käänny oikealle
-        tracker:turnRight()
-        -- kaiva 3 eteenpäin
-        for i = 1, 3 do
-            turtle.dig()
-            tracker:moveForward()
-        end
-        -- käänny alkuperäiseen suuntaan
         tracker:turnRight()
     end)
 end
