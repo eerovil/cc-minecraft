7d8a9e519a61e45bf873e698e2b6a549c64bb079
Eero Vilpponen <eero.vilpponen@gmail.com>
2025-10-26 17:04:48 +0200

    simple

--- melee.lua
+++ melee.lua
@@ -1,14 +1,4 @@
 local utils = dofile("lib/utils.lua")
-local Actions = dofile("lib/actions.lua")
-local SuoniKaivaja = dofile("lib/ore.lua")
-tracker = Actions.new(utils.getLabel())
 
-while true do 
-    tracker:cycle(function() 
-        -- lyö ja käänny
-        turtle.attack()
-        tracker:turnRight()
-    end)
-end
-
-shell.run("resetstate.lua")
+turtle.attack()
+turtle.turnRight()
