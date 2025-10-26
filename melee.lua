a29094d91387c13d0ad633edec74231c984cbdd6
Eero Vilpponen <eero.vilpponen@gmail.com>
2025-10-26 17:12:02 +0200

    add drop

--- melee.lua
+++ melee.lua
@@ -1,5 +1,15 @@
 local utils = dofile("lib/utils.lua")
 
+local function selectSlotWithAnyItem()
+  for slot = 1, 16 do
+    if turtle.getItemCount(slot) > 0 then
+      turtle.select(slot)
+      return true
+    end
+  end
+  return false
+end
+
 while true do
     for i = 1, 8 do
         turtle.attack()
@@ -8,4 +18,6 @@ while true do
     turtle.back()
     turtle.attack()
     turtle.forward()
+    turtle.dropDown()
+    selectSlotWithAnyItem()
 end
